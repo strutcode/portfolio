c9f7eaa8b8123ed77a28c447e3818815a1ad71d1
Tristan Shelton <tristan.m.shelton@gmail.com>
2019-02-03 12:12:24 -0500

    Add tailswinds and configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,30 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    semi: ['error', 'never'],
+    'import/no-extraneous-dependencies': [
+      'off',
+      { devDependencies: ['**/*.config.js', '**/*.spec.[jt]s'] },
+    ],
   },
+  overrides: [
+    {
+      files: ['*.config.js'],
+      rules: {
+        'global-require': 'off',
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 0,
+        'vue/html-indent': ['error', 2, { baseIndent: 1 }],
+        'vue/script-indent': ['error', 2, { baseIndent: 1, switchCase: 1 }],
+      },
+    },
+  ],
   parserOptions: {
     parser: '@typescript-eslint/parser',
   },
-};
+}

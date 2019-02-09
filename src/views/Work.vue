472def8ccf1fd733969d086d6179945f5d878b0c
Tristan Shelton <tristan.m.shelton@gmail.com>
2019-02-09 09:09:22 -0500

    Add content to work page

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="work page">
     <div class="container content">
-      <div class="work-grid">
-        <div v-for="i in 9" :key="i">
-          <a href="#" class="item" />
+      <div class="work-carousel">
+        <div class="item" v-for="entry in entries" :key="entry.title">
+          <div class="graphic">
+            <div class="image">
+              <img :src="entry.img" />
+            </div>
+          </div>
+          <div class="info">
+            <div v-html="entry.info" />
+            <button>Visit the Site</button>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator'
+
+  interface Entry {
+    img: string,
+    info: string,
+  }
+
+  @Component
+  export default class Work extends Vue {
+    /* eslint-disable global-require */
+    entries: Entry[] = [
+      {
+        img: require('@/assets/jet-blue.jpg'),
+        info: require('@/assets/jet-blue.md'),
+      },
+      {
+        img: require('@/assets/a-natural-chef.jpg'),
+        info: require('@/assets/a-natural-chef.md'),
+      },
+      {
+        img: require('@/assets/something-fabulous.jpg'),
+        info: require('@/assets/something-fabulous.md'),
+      },
+      {
+        img: require('@/assets/bali-childrens-project.jpg'),
+        info: require('@/assets/bali-childrens-project.md'),
+      },
+      {
+        img: require('@/assets/rondout-valley-growers.jpg'),
+        info: require('@/assets/rondout-valley-growers.md'),
+      },
+    ]
+  }
+</script>
+
+
 <style lang="sass" scoped>
   .content
     padding-top: 10%
+
+  .work-carousel
+    // @apply transform-center
 
   .work-grid
     @apply flex flex-wrap
@@ -20,19 +68,47 @@
     & > div
       @apply w-full p-4
 
-      .item
-        @apply block bg-grey-light
+  .item
+    @apply absolute flex bg-white
+
+    .graphic
+      flex: 0 0 50%
+
+      .image
+        @apply relative overflow-hidden shadow-lg
+        transform: perspective(1000px) rotateY(15deg)
 
         &:before
+          @apply block
           content: ''
-          display: block
-          padding-top: 56.25%
+          padding-top: 100%
 
-  @screen md
-    .work-grid > div
-      @apply w-1/2
+        img
+          @apply block absolute pin-t pin-l
 
-  @screen lg
-    .work-grid > div
-      @apply w-1/3
+    & /deep/ .info
+      @apply pl-12
+
+      h2, h3
+        @apply text-grey-darker
+
+      h3
+        @apply mt-4
+
+      ul
+        @apply my-4
+
+        li
+          @apply text-grey-darker p-1
+
+      button
+        @apply bg-teal py-4 px-12 mt-8 text-grey-lightest rounded shadow
+
+  // @screen md
+  //   .work-grid > div
+  //     @apply w-1/2
+
+  // @screen lg
+  //   .work-grid > div
+  //     @apply w-1/3
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 module.exports = {
+  // chainWebpack(config) {
+  //   config.module.rule('md')
+  //     .test(/\.md$/)
+  //     .use('markdown-loader')
+  //     .loader(['html-loader', 'markdown-loader'])
+  // },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.md$/,
+          use: ['html-loader', 'markdown-loader'],
+        },
+      ],
+    },
+  },
   css: {
     loaderOptions: {
       sass: {
